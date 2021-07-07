922fa2f0caea68da9c7dcd3c5f1f45208dd3f216
Shubham Bhawsar <shubham.b.interns@aux.gojek.com>
2021-07-07 15:41:31 +0530

    UPDATE: varaible PORT updates

--- main.go
+++ main.go
@@ -5,13 +5,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
 	r := router.Router()
-	var port string
-	fmt.Println("Enter the PORT: ")
-	fmt.Scanln(&port)
 	fmt.Printf("Starting server on the port %s...", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), r))
 }
