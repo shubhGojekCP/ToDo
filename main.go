c611e260a6f5c561fd87c23e93cf69a38c6e97eb
Shubham Bhawsar <shubham.b.interns@aux.gojek.com>
2021-07-11 16:08:05 +0530

    UPDATE: Graceful ShutDown

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"ToDo/router"
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -17,6 +20,28 @@ func main() {
 	}
 	port := os.Getenv("PORT")
 	r := router.Router()
-	fmt.Printf("Starting server on the port %s...", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), r))
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: r,
+	}
+	ctx, _ := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("listen: %s\n", err)
+		}
+	}()
+	log.Print("Server Started")
+
+	<-ctx.Done()
+	log.Print("Server Stopped")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Printf("Server Shutdown Failed:%+v", err)
+	} else {
+		log.Print("Server Exited Properly")
+	}
 }
